65f35afa2de22c29f4c544b5d0dde0c832142dc4
Dimitri Leonzon <dimitri.leonzon@gmail.com>
2025-11-09 12:08:50 -0500

    Module 11 Assignment

---
+++ module-11-assignment.R
@@ -0,0 +1,25 @@
+## Dimitri Leonzon
+## LIS 4370
+## Module 11 Assignment
+## blog link: 
+
+install.packages("CarletonStats")
+library(CarletonStats)
+
+
+corrected_tukey <- function(x) {
+  outliers <- array(TRUE, dim = dim(x))
+  for (j in seq_len(ncol(x))) {
+    # element-wise AND instead of scalar &&
+    outliers[, j] <- outliers[, j] & tukey.outlier(x[, j])
+  }
+  outlier.vec <- logical(nrow(x))
+  for (i in seq_len(nrow(x))) {
+    outlier.vec[i] <- all(outliers[i, ])
+  }
+  outlier.vec
+}
+
+set.seed(123)
+test_mat <- matrix(rnorm(50), nrow = 10)
+corrected_tukey(test_mat)
